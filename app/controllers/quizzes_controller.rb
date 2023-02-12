8b733eb8d906beb4810d79b57569a4471d875c44
ItohMarina <117239917+itohmarina@users.noreply.github.com>
2023-02-12 14:10:37 +0000

    add views and routing

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,32 @@
 class QuizzesController < ApplicationController
   
   def show
+    @quiz = Quiz.order("RAND()").limit(1)
   end
   
+  def new
+    @quiz = Quiz.new
+  end
+  
+  def create
+    unless params[:answer_image].attached?
+      render "quizzes/new"
+    end 
+    @quiz = Quiz.new
+    unless @quiz.save(quiz_params)
+      render "quizzes/new"
+    end 
+  end
+  
+  def index
+    @quizzes = Quiz.all
+  end 
+  
+  
+  private
+  
+  def quiz_params
+    params.require(:quiz).permit(:title, :body, :answer_image)
+  end 
+
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,3 @@
 class Quiz < ApplicationRecord
+  has_one_attached :answer_image
 end
